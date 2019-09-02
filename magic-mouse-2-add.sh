ded0ac0d05eaf102d6dc43c9d4126e17a2021061
Dan Abad <dan.r.abad@gmail.com>
2019-09-02 18:49:19 -0500

    Update kernel version

--- magic-mouse-2-add.sh
+++ magic-mouse-2-add.sh
@@ -2,7 +2,7 @@
 
 reload() {
     modprobe -r hid_magicmouse
-    insmod /home/dan/.local/lib/modules/5.0.0-25-generic/extra/hid-magicmouse.ko \
+    insmod /home/dan/.local/lib/modules/5.0.0-27-generic/extra/hid-magicmouse.ko \
            scroll_acceleration=1 \
            scroll_speed=43 \
            middle_click_3finger=1
